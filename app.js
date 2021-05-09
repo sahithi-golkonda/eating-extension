8b78032420e205b58309c1c6fe3f06f30c06d246
aditi-j <aditiawe1312@gmail.com>
2021-05-08 19:38:42 -0500

    notification works

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
-console.log('Greet bot!');
+// console.log('Greet bot!');
 
 const button = document.createElement('button');
 button.textContent = 'Greet me!'
 document.body.insertAdjacentElement('afterbegin', button);
 button.addEventListener('click', () => {
-  chrome.runtime.sendMessage('', {
-    type: 'notification',
-    options: {
-      title: 'Just wanted to notify you',
-      message: 'How great it is!',
-    //   iconUrl: '/icon.png',
-      type: 'basic'
-    }
-  });
+  // chrome.runtime.sendMessage('', {
+  //   type: 'notification',
+  //   options: {
+  //     title: 'Just wanted to notify you',
+  //     message: 'How great it is!',
+  //     iconUrl: 'icon:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7',
+  //     type: 'basic'
+  //   }
+  // });
+  notifyMe();
 });
+
+function notifyMe() {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("Start cooking!!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("Start cooking!!");
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them any more.
+}
