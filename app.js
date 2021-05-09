2a13724ef8cd65ce9d38672092be15045af23a6c
aditi-j <aditiawe1312@gmail.com>
2021-05-08 21:14:37 -0500

    final

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
 // console.log('Greet bot!');
 
 const button = document.createElement('button');
-button.textContent = 'Greet me!'
+button.textContent = 'I\'m hungry!'
 document.body.insertAdjacentElement('afterbegin', button);
+
+
+// button.addEventListener('click', () => {
+  
+//   notifyMe();
+  
+// });
+
 button.addEventListener('click', () => {
-  // chrome.runtime.sendMessage('', {
-  //   type: 'notification',
-  //   options: {
-  //     title: 'Just wanted to notify you',
-  //     message: 'How great it is!',
-  //     iconUrl: 'icon:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7',
-  //     type: 'basic'
-  //   }
-  // });
-  notifyMe();
+  
+  notifyMeRestraurant();
+  
 });
+
+// Listen
+// chrome.alarms.onAlarm.addListener(function(alarm) {
+//   if (alarm.name === 'cook') {
+//       // Whatever you want
+//       notifyMe();
+//   }
+// });
+
+// function createAlarm() {
+//   var now = new Date();
+//   var day = now.getDate();
+//   if (now.getHours() >= 20) {
+//       // 3 AM already passed
+//       day += 1;
+//   }
+//   // '+' casts the date to a number, like [object Date].getTime();
+//   var timestamp = +new Date(now.getFullYear(), now.getMonth(), day, 20, 11, 0, 0);
+//   //                        YYYY               MM              DD  HH MM SS MS
+
+//   // Create
+//   chrome.alarms.create('cook', {
+//       when: timestamp
+//   });
+// }
+
+// createAlarm();
 
 function notifyMe() {
   // Let's check if the browser supports notifications
@@ -24,7 +52,7 @@ function notifyMe() {
 
   // Let's check whether notification permissions have already been granted
   else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
+
     var notification = new Notification("Start cooking!!");
   }
 
@@ -34,6 +62,48 @@ function notifyMe() {
       // If the user accepts, let's create a notification
       if (permission === "granted") {
         var notification = new Notification("Start cooking!!");
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them any more.
+}
+
+async function notifyMeRestraurant() {
+  // get restraurant
+  var requestOptions = {
+    method: 'GET',
+    headers: {"Authorization": "Bearer KZmYsVRbiqJSyL9mcTwWvgGdXEcv_GDMg18qmZep_1HAmlkzzXdNctyWwh_aFdt_qOAM8RSpRZls_LytXaMqo9EBojJkPakAO4jq1r2EkeAN1_yFDORS8K60b9uWYHYx"},
+    redirect: 'follow'
+  };
+
+  let proxy = "https://limitless-retreat-60020.herokuapp.com/";
+  let url = "https://api.yelp.com/v3/businesses/search?open_now=True&location="
+
+  let jsonData = await fetch(proxy + url + "Austin", requestOptions);
+  let data = await jsonData.json();
+  let restaurants = await data["businesses"];
+  let randomNum = Math.floor(Math.random() * restaurants.length);
+  let restaurant = restaurants[randomNum];
+
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+
+    var notification = new Notification(restaurant["name"]);
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification(restaurant["name"]);
       }
     });
   }
--- background.js
+++ background.js
@@ -11,40 +11,6 @@ chrome.runtime.onMessage.addListener(data => {
   }
 });
 
-// chrome.runtime.onMessage.addListener(data => {
-//   if (data.type === 'notification') {
-//     chrome.notifications.create('', data.options);
-//     // chrome.notifications.create('test', {
-//     //   type: 'basic',
-//     //   // iconUrl: 'images/1.png',
-//     //   title: 'Test Message',
-//     //   message: 'You are awesome!',
-//     //   priority: 2,
-//     //   eventTime: Date.now()
-//     // });
-//   }
-// });
-
-// var Notification=(function(){
-//   var notification=null;
-
-//   return {
-//       display:function(opt){
-//           notification=chrome.notifications.create(opt);
-//           notification.show();
-//       },
-//       hide:function(){
-//           notification.close();
-//       }
-//   };
-// })();
-
-// chrome.browserAction.onClicked.addListener(function(windowId){
-//   var opt = {
-//       type: "basic",
-//       title: "PixelsTech Website Statistic Viewer",
-//       message: "1,000,000",
-//       // iconUrl: "pixelstech.gif"
-//   };
-//   Notification.display(opt);
-// });
+chrome.alarms.onAlarm.addListener(function( alarm ) {
+  console.log("Got an alarm!", alarm);
+});
